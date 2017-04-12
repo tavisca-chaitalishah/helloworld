4eba6556449070f8059f2e84103f0e7488836440
tavisca-chaitalishah <cshah@tavisca.com>
2017-04-12 19:21:30 +0530

    code

---
+++ Chat Server/chat server local/ChattingApplication/ChattingApplication/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChattingApplication
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadKey();
+        }
+
+        private static void SetupServer()
+        {
+            Console.Write("Setting Up Server..");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.Write("Client Connects..");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+             
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuff = new byte[received];
+            Array.Copy(_buffer, dataBuff, received);
+            string text = Encoding.ASCII.GetString(dataBuff);
+            Console.WriteLine("Text Received : " + text);
+            string response = string.Empty;
+            if(text.ToLower() != "get time")
+            {
+               response = "Invalid Request"; 
+            }
+            else
+            {
+                response=  DateTime.Now.ToLongTimeString();
+            }
+           
+           
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+       
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+
+
+}
---
+++ Chat Server/chat server local/ClientSide/ClientSide/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientSide
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            sendLoop();
+            Console.ReadLine();
+        }
+
+        private static void sendLoop()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                byte[] buffer = Encoding.ASCII.GetBytes("get time");
+                _clientSocket.Send(buffer);
+                byte[] receivedBuf = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuf);
+
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuf, data, rec);
+                Console.WriteLine("Received :" + Encoding.ASCII.GetString(data));
+                
+            }
+        }
+
+        private static void LoopConnect()
+        {
+
+            int attemps = 0;
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attemps++;
+
+                    _clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection Attemps :" + attemps.ToString());
+
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+    }
+}
